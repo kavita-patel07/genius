3e96e1846b0b85d2b195960d694692baac7f151d
dipti-kumari11 <114990043+dipti-kumari11@users.noreply.github.com>
2024-03-06 11:50:34 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 class hello{
   public static void main(String args[]){
     int a=5;
-    int b=9;
+    int b=16;
     System.out.println("Hello World);
     System.out.println(b-a);
     System.out.println(a+b);
